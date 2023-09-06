25abf90d9b3b42aa1b3a58e3767779138af96821
ercument0377 <ercument0377@gmail.com>
2023-09-06 23:27:14 +0300

    ilyas

---
+++ src/test/java/pages/OperatingRoomProcedures_Page.java
@@ -0,0 +1,224 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class OperatingRoomProcedures_Page {
+    public OperatingRoomProcedures_Page(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "(//a[@class='open'])[3]")
+    public WebElement hastaIslemleri;
+
+    @FindBy(xpath = "//*[@id=\"menuDurumDegis\"]/div[2]/div/ul/li/div/ul/li[2]/a")
+    public WebElement ameliyathaneIslemleri1;
+
+    @FindBy(xpath = "//*[@id=\"menuDurumDegis\"]/div[2]/div/ul/li/div/ul/li[2]/div/ul/li[2]/a")
+    public WebElement ameliyathaneIslemleri2;
+
+
+    @FindBy(xpath = "//*[@id=\"AMELIYAT_LST_TARIH\"]")
+    public WebElement requestDate;
+
+    @FindBy(xpath = "//*[@id=\"btnLstYenile\"]")
+    public WebElement refreshButton;
+
+    @FindBy(xpath = "//td[text()='Salon_4']")
+    public WebElement selectHallName;
+
+
+    @FindBy(xpath = "/html/body/div[1]/div[4]/section/div[4]/div[2]/div[2]/div[2]/table/tbody/tr/td/div[2]/table/tbody/tr[3]/td[3]")
+    public WebElement protocolNo;
+
+    @FindBy(xpath = "//a[contains(text(),'Salon İşlemleri')]")
+    public WebElement HallOperations;
+
+    @FindBy(xpath = "//a[contains(text(),'Salon Onayla')]")
+    public WebElement hallApprove ;
+
+    @FindBy(xpath = "//*[@id=\"onayIcerik\"]/div/div[3]/span[1]")
+    public WebElement DoYouWantToContinuePopupYESbutton;
+
+    @FindBy(xpath = "//*[@id=\"menuDurumDegis\"]/div[2]/div/ul/li/div/ul/li[2]/div/ul/li[3]/a")
+    public WebElement doctorSurgeryProceduresPage ;
+
+
+    @FindBy(xpath = "//*[@id=\"salon_durumu\"]")
+    public WebElement hallStatus ;
+
+    @FindBy(xpath = "//a[text()='Onay İptal']")
+    public WebElement hallCanceled;
+
+    @FindBy(xpath = "//td[text()='Salon Adı']")
+    public WebElement hallName;
+
+    @FindBy(xpath = "//td[@id='AMELIYAT_SALON_LISTESI_tccell3_1']//b[1]")
+    public WebElement hallNameTik;
+
+
+    @FindBy(xpath = "//a[contains(text(),'Tüm Salonları Onayla')]")
+    public WebElement AllhallApprove;
+
+
+    @FindBy(xpath = "//a[contains(text(),'Tüm Salon Onay İptal')]")
+    public WebElement AllhallCancelled;
+
+    @FindBy(xpath = "//*[@id=\"dxGridAmeliyatHastaListesi_tccell0_0\"]")
+    public WebElement patientOperations;
+
+    @FindBy(xpath = "/html/body/div[7]/div/a[2]")
+    public WebElement changeHall;
+
+
+
+
+    @FindBy(xpath = "//a[contains(text(),'Seç')]")
+    public WebElement chooseNewHall;
+
+
+
+    @FindBy(xpath = "//*[@id=\"protokolAraInput\"]")
+    public WebElement protocolSearch ;
+
+    @FindBy(xpath = "//a[contains(text(),'Hasta Bul')]")
+    public WebElement findPatient ;
+
+    @FindBy(xpath = "//a[contains(text(),'Ameliyat Detayları')]")
+    public WebElement operationDetails ;
+
+    @FindBy(xpath = "//*[@id=\"AMELIYAT_TANI\"]")
+    public WebElement operationDiagnosis ;
+
+   @FindBy(xpath = "//*[@id=\"AmeliyatDetaylariPopup\"]/div[2]/div[2]/div[2]/div[1]/div[1]/h2/div/a")
+    public WebElement addDiagnosis ;
+
+    @FindBy(xpath = "//*[@id=\"TaniListesiLookup_tcFC2\"]/input")
+    public WebElement sellectAllDiagnosis ;
+
+    @FindBy(xpath = "//*[@id=\"TaniListesiLookup_tcheader2\"]/a")
+    public WebElement selelctAndCloseButton ;
+
+
+    @FindBy(xpath = "//*[@id=\"AMELIYAT_ADI\"]")
+    public WebElement operationName ;
+
+    @FindBy(xpath = "//*[@id=\"AMELIYAT_ACIKLAMA\"]")
+    public WebElement operationDescription ;
+
+    @FindBy(xpath = "//*[@id=\"AMELIYAT_YATIS_NOTU\"]")
+    public WebElement hospitalizationNote ;
+
+    @FindBy(xpath = "//*[@id=\"PopupSaveButton_0\"]/span")
+    public WebElement saveOperationDetailsButton;
+
+
+    @FindBy(xpath = "//a[contains(text(),'Hasta Ameliyathaneye Giriş Yapıldı')]")
+    public WebElement operatingRoomEntranceButton;
+
+    @FindBy(xpath = "//*[@id=\"dxGridAmeliyatHastaListesi_tccell0_9\"]/div")
+    public WebElement statusButton;
+
+
+    @FindBy(xpath = "/html/body/div[1]/div[4]/section/div[4]/div[2]/div[2]/div[6]/a[2]")
+    public WebElement operatingRoomCancelsEntranceButton;
+
+
+    @FindBy(xpath = "//*[@id=\"input_ayaktahasta_protokol\"]")
+    public WebElement popup1protokolNo;
+
+    @FindBy(xpath = "//*[@id=\"AyaktaHastaPopup\"]/div[2]/div[2]/div[1]/a[1]")
+    public WebElement protokolNoQuestioningButton;
+
+    @FindBy(xpath = "/html/body/div[7]/div[2]/div[2]/div[1]/div/div[2]/input[3]")
+    public WebElement popup1selectSalonButton;
+
+    @FindBy(xpath = "//*[@id=\"lstSalonMasaListesi_tccell0_3\"]/a")
+    public WebElement selectSalonButtonFromList;
+
+    @FindBy(xpath = "/html/body/div[1]/div[4]/section/div[4]/div[1]/div[2]/a[2]")
+    public WebElement addPatientListButton;
+
+    @FindBy(xpath = "//*[@id=\"btn-ayaktahastaekle\"]")
+    public WebElement popup1addPatientListButton;
+
+    @FindBy(xpath = "/html/body/div[1]/div[4]/section/div[4]/div[1]/div[2]/a[4]")
+    public WebElement POstopButton;
+
+    @FindBy(xpath = "//*[@id=\"dxGridAmeliyatPostop_DXFREditorcol0_I\"]")
+    public WebElement POstopProtokolNoButton;
+
+    @FindBy(xpath = "//*[@id=\"dxGridAmeliyatPostop_DXDataRow1\"]/td[1]")
+    public WebElement POstopSelectPatient;
+
+
+    @FindBy(xpath = "//*[@id=\"PopupAmeliyatPostop\"]/div[2]/div[2]/div/div[1]/div[3]/a")
+    public WebElement POstopSEndPatientServiceButton;
+
+    @FindBy(xpath = "//*[@id=\"POSTOP_TARIH\"]")
+    public WebElement POstopPageDataButton;
+
+    @FindBy(xpath = "//*[@id=\"PopupAmeliyatPostop\"]/div[2]/div[1]/a/span")
+    public WebElement POstopCloseButton;
+
+
+    @FindBy(xpath = "/html/body/div[7]/div[2]/div[2]/div/div[1]/a")
+    public WebElement POstopPageRefreshButton;
+
+
+    @FindBy(xpath = "//*[@id=\"dxGridAmeliyatHastaListesi_tccell0_9\"]/div")
+    public WebElement serviceStatus;
+
+    @FindBy(xpath = "//*[@id=\"9de3c0dc_8766_4f34_ad56_cde74254a2b6\"]/input[3]")
+    public WebElement Popup1selectSalonButton;
+
+    @FindBy(xpath = "//*[@id=\"lstSalonMasaListesi_tccell1_3\"]/a")
+    public WebElement Popup2selectSalonButton;
+
+
+    @FindBy(xpath = "//*[@id=\"ServisListe_tccell1_2\"]/a")
+    public WebElement EMERGENCYCHILDDAYCAREServiceButton;
+
+
+
+
+
+    @FindBy(xpath = "//*[@id=\"64ce4f9a_bdbf_4e4f_a6db_f4b9d5c558d8\"]/input[3]")
+    public WebElement Popup1HallButton;
+
+    @FindBy(xpath = "/html/body/div[7]/div[2]/div[2]/div[1]/div/div[1]/input[3]")
+    public WebElement popup1serviceButton;
+
+
+    @FindBy(xpath = "//*[@id=\"protokolAraInput\"]")
+    public WebElement homePageProtokolButton;
+
+    @FindBy(xpath = "//*[@id=\"salon_servis_adi\"]")
+    public WebElement homePageServiceName;
+
+    @FindBy(xpath = "/html/body/div[7]/div/a[1]")
+    public WebElement takeOfTableButton;
+
+
+    @FindBy(xpath = "//*[@id=\"PopupSaveButton_0\"]/span")
+    public WebElement takeOfTablepageAddButton;
+
+    @FindBy(xpath = "//*[@id=\"ServisListe_DXFREditorcol0_I\"]")
+    public WebElement poupServisNameButton;
+
+    @FindBy(xpath = "//*[@id=\"ServisListe_tccell0_2\"]/a")
+    public WebElement poupKadınDogumServiceButton;
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/stepdefinitions/OperatingRoomProcedures_StepDefinitions.java
@@ -0,0 +1,453 @@
+package stepdefinitions;
+import com.github.javafaker.Faker;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.asserts.Assertion;
+import pages.OperatingRoomProcedures_Page;
+import pages.BasePage;
+import pages.Login_Page;
+import utilities.Driver;
+
+
+public class OperatingRoomProcedures_StepDefinitions {
+
+    Login_Page login_page = new Login_Page();
+    BasePage basePage = new BasePage();
+    Actions actions = new Actions(Driver.getDriver());
+
+    OperatingRoomProcedures_Page operatingRoomProcedures_page_ = new OperatingRoomProcedures_Page();
+    Faker faker = new Faker();
+
+
+    @Given("The user goes Hasta islemleri, Ameliyathane islemleri and then click Ameliyathane islemleri")
+    public void the_user_goes_hasta_islemleri_ameliyathane_islemleri_and_then_click_ameliyathane_islemleri() {
+        actions.moveToElement(basePage.hastaIslemleri).
+                moveToElement(basePage.ameliyathaneIslemleri1).
+                moveToElement(basePage.ameliyathaneIslemleri2).
+                perform();
+        basePage.ameliyathaneIslemleri2.click();
+    }
+
+    @Given("User enters date")
+    public void user_enters_date() throws InterruptedException {
+        Thread.sleep(2000);
+        operatingRoomProcedures_page_.requestDate.click();
+        operatingRoomProcedures_page_.requestDate.sendKeys("15.09.2023");
+        Thread.sleep(2000);
+
+    }
+
+    @Given("User clicks the refresh button")
+    public void user_clicks_the_refresh_button() throws InterruptedException {
+        operatingRoomProcedures_page_.refreshButton.click();
+        Thread.sleep(1000);
+
+    }
+
+    @Given("User selects hall")
+    public void user_selects_hall() throws InterruptedException {
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.selectHallName.click();
+        Thread.sleep(1000);
+
+    }
+
+    @Given("User selects patient")
+    public void user_selects_patient() throws InterruptedException {
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.protocolNo.click();
+        Thread.sleep(1000);
+
+    }
+
+    @Given("User clicks on salon operations")
+    public void user_clicks_on_salon_operations() throws InterruptedException {
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.HallOperations.click();
+        Thread.sleep(1000);
+
+    }
+
+    @Given("User hall approves")
+    public void user_hall_approves() throws InterruptedException {
+        operatingRoomProcedures_page_.hallApprove.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.DoYouWantToContinuePopupYESbutton.click();
+        Thread.sleep(1000);
+
+    }
+
+    @Given("The user goes Hasta islemleri, Ameliyathane islemleri and then click doktor ameliyathane islemleri")
+    public void the_user_goes_hasta_islemleri_ameliyathane_islemleri_and_then_click_doktor_ameliyathane_islemleri() {
+        actions.moveToElement(basePage.hastaIslemleri).
+                moveToElement(basePage.ameliyathaneIslemleri1).
+                moveToElement(operatingRoomProcedures_page_.doctorSurgeryProceduresPage).
+                perform();
+        operatingRoomProcedures_page_.doctorSurgeryProceduresPage.click();
+    }
+
+    @Given("User verifies patient")
+    public void user_verifies_patient() {
+        Assertion assertion = new Assertion();
+        Assert.assertEquals("Salon onaylanmıştır.", operatingRoomProcedures_page_.hallStatus.getText());
+        org.junit.Assert.assertTrue("Bilgiler kaydedildi.", true);
+
+    }
+
+    @Given("User cancels the salon")
+    public void user_cancels_the_salon() throws InterruptedException {
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.hallCanceled.click();
+
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.DoYouWantToContinuePopupYESbutton.click();
+        Thread.sleep(1000);
+
+    }
+
+    @Given("User verify that the hall has been cancelled")
+    public void user_confirms_that_the_hall_has_been_cancelled() throws InterruptedException {
+        Thread.sleep(1000);
+        Assert.assertFalse(operatingRoomProcedures_page_.hallStatus.getText().contains("Salon onaylanmıştır."));
+
+
+    }
+
+    @Given("User confirms all halls")
+    public void user_confirms_all_halls() throws InterruptedException {
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.HallOperations.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.AllhallApprove.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.DoYouWantToContinuePopupYESbutton.click();
+        Thread.sleep(1000);
+    }
+
+    @Given("User cancels all halls")
+    public void user_cancels_all_halls() throws InterruptedException {
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.HallOperations.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.AllhallCancelled.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.DoYouWantToContinuePopupYESbutton.click();
+        Thread.sleep(1000);
+    }
+
+    @Given("The user clicks on the patient operations button")
+    public void the_user_clicks_on_the_patient_operations_button() throws InterruptedException {
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.patientOperations.click();
+
+    }
+
+    @Given("User clicks on change hall button")
+    public void user_clicks_on_change_hall_button() {
+        operatingRoomProcedures_page_.changeHall.click();
+
+    }
+
+    @Given("User selects new hall")
+    public void user_selects_new_hall() throws InterruptedException {
+        operatingRoomProcedures_page_.chooseNewHall.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.DoYouWantToContinuePopupYESbutton.click();
+        Thread.sleep(1000);
+
+
+    }
+
+    @Given("user finds patient with protocol number")
+    public void user_finds_patient_with_protocol_number() throws InterruptedException {
+
+        operatingRoomProcedures_page_.protocolSearch.click();
+        operatingRoomProcedures_page_.protocolSearch.sendKeys("15384267");
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.findPatient.click();
+
+    }
+/*
+    @Given("The user clicks on the hall  has selected")
+    public void the_user_clicks_on_the_hall_he_she_has_selected() {
+
+
+    }
+    @Given("Confirms that the user left has changed")
+    public void confirms_that_the_user_left_has_changed() {
+
+    }
+*/
+
+
+    @Given("User clicks on the surgery details button")
+    public void user_clicks_on_the_surgery_details_button() {
+        operatingRoomProcedures_page_.operationDetails.click();
+    }
+
+    @Given("User selects Diagnosis")
+    public void user_selects_diagnosis() {
+        operatingRoomProcedures_page_.operationDiagnosis.click();
+
+    }
+
+    @Given("User writes diagnosis")
+    public void user_writes_diagnosis() throws InterruptedException {
+        operatingRoomProcedures_page_.operationDiagnosis.sendKeys("Merhaba bu bir deneme yayınıdır.Şimdi  buraya AMELİYAT TANISI listesinden bütün tanıları seçeceğim.");
+        Thread.sleep(1000);
+
+        operatingRoomProcedures_page_.addDiagnosis.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.sellectAllDiagnosis.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.selelctAndCloseButton.click();
+        Thread.sleep(1000);
+
+
+    }
+
+    @Given("User writes description")
+    public void user_writes_description() throws InterruptedException {
+        operatingRoomProcedures_page_.operationDescription.click();
+        operatingRoomProcedures_page_.operationDescription.sendKeys("Merhaba bu bir deneme yayınıdır. buraya ACIKLAMA girilmelidir.");
+        Thread.sleep(1000);
+    }
+
+    @Given("User writes a hospitalization note")
+    public void user_writes_a_hospitalization_note() throws InterruptedException {
+        operatingRoomProcedures_page_.hospitalizationNote.click();
+        operatingRoomProcedures_page_.hospitalizationNote.sendKeys("Merhaba bu bir deneme yayınıdır. buraya YATIS NOTU girilmelidir.");
+        Thread.sleep(1000);
+    }
+
+    @Given("User clicks the Save button on the Surgery details page")
+    public void user_clicks_the_save_button_on_the_surgery_details_page() {
+        operatingRoomProcedures_page_.saveOperationDetailsButton.click();
+
+    }
+
+    @Given("User clicks on the button Patient entered the operating room")
+    public void user_clicks_on_the_button_patient_entered_the_operating_room() throws InterruptedException {
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        operatingRoomProcedures_page_.operatingRoomEntranceButton.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.DoYouWantToContinuePopupYESbutton.click();
+        Thread.sleep(1000);
+
+        actions.sendKeys(Keys.PAGE_UP).perform();
+
+        Thread.sleep(1000);
+
+
+    }
+ /*   @Given("The user verifies the patient's entry into the operating room")
+    public void the_user_verifies_the_patient_s_entry_into_the_operating_room() {
+
+        Assert.assertTrue(ameliyathaneIslemleri_page_i.statusButton.getText().contains("Ameliyathaneye İndirildi"));
+    //    Assert.assertEquals("Ameliyathaneye İndirildi",ameliyathaneIslemleri_page_i.statusButton.getText());
+    }
+*/
+
+
+    @Given("User cancels the patient's entrance to the operating room of the patient")
+    public void user_cancels_the_patient_s_entrance_to_the_operating_room_of_the_patient() throws InterruptedException {
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        operatingRoomProcedures_page_.operatingRoomCancelsEntranceButton.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.DoYouWantToContinuePopupYESbutton.click();
+        Thread.sleep(1000);
+        actions.sendKeys(Keys.PAGE_UP).perform();
+
+    }
+
+    @Given("User clicks the patient operating room login cancel button")
+    public void user_clicks_the_patient_operating_room_login_cancel_button() throws InterruptedException {
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        operatingRoomProcedures_page_.operatingRoomCancelsEntranceButton.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.DoYouWantToContinuePopupYESbutton.click();
+    }
+
+    @Given("User clicks on Add patient to list button")
+    public void user_clicks_on_add_patient_to_list_button() throws InterruptedException {
+        operatingRoomProcedures_page_.addPatientListButton.click();
+        Thread.sleep(1000);
+
+    }
+
+    @Given("The user enters the protocol number of the patient they want to add")
+    public void the_user_enters_the_protocol_number_of_the_patient_they_want_to_add() throws InterruptedException {
+        operatingRoomProcedures_page_.popup1protokolNo.click();
+        operatingRoomProcedures_page_.popup1protokolNo.sendKeys("15384267");
+        Thread.sleep(1000);
+
+    }
+
+    @Given("User clicks on the questioning button")
+    public void user_clicks_on_the_Questioning_button() {
+        operatingRoomProcedures_page_.protokolNoQuestioningButton.click();
+
+
+    }
+
+    @Given("User clicks on the poup1 salon button")
+    public void user_clicks_on_the_poup1_salon_button() throws InterruptedException {
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.popup1selectSalonButton.click();
+        Thread.sleep(1000);
+
+    }
+
+    @Given("User popup2 selects hall")
+    public void user_popup2_selects_hall() {
+        operatingRoomProcedures_page_.selectSalonButtonFromList.click();
+
+    }
+
+    @Given("User clicks popup1 on Add patient to list button")
+    public void user_clicks_popup1_on_add_patient_to_list_button() {
+        operatingRoomProcedures_page_.popup1addPatientListButton.click();
+    }
+
+    @Given("User clicks the POstop button")
+    public void user_clicks_the_p_ostop_button() throws InterruptedException {
+        operatingRoomProcedures_page_.POstopButton.click();
+        Thread.sleep(1000);
+
+    }
+
+    @Given("User enter data POstop page")
+    public void user_enter_data_p_ostop_page() throws InterruptedException {
+        operatingRoomProcedures_page_.POstopPageDataButton.click();
+        operatingRoomProcedures_page_.POstopPageDataButton.sendKeys("15.09.2023" + Keys.ENTER);
+        Thread.sleep(1000);
+    }
+
+    @Given("User click the refresh button POstop page")
+    public void user_click_the_refresh_button_p_ostop_page() {
+        operatingRoomProcedures_page_.POstopPageRefreshButton.click();
+
+    }
+
+    @Given("User enter protokol number Postop page")
+    public void user_enter_protokol_number_postop_page() throws InterruptedException {
+        operatingRoomProcedures_page_.POstopProtokolNoButton.click();
+        operatingRoomProcedures_page_.POstopProtokolNoButton.sendKeys("15384267" + Keys.ENTER);
+        Thread.sleep(1000);
+
+    }
+
+    @Given("The user selects the patient they want to send to the service")
+    public void the_user_selects_the_patient_they_want_to_send_to_the_service() throws InterruptedException {
+        Thread.sleep(3000);
+        operatingRoomProcedures_page_.POstopSelectPatient.click();
+        Thread.sleep(2000);
+
+    }
+
+    @Given("User clicks on Send patient to ward button")
+    public void user_clicks_on_send_patient_to_ward_button() throws InterruptedException {
+        operatingRoomProcedures_page_.POstopSEndPatientServiceButton.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.POstopCloseButton.click();
+
+    }
+
+    @Given("User verifies that the patient has been sent to the service")
+    public void user_verifies_that_the_patient_has_been_sent_to_the_service() {
+        Assertion assertion = new Assertion();
+        Assert.assertEquals("Servise Gönderildi", operatingRoomProcedures_page_.serviceStatus.getText());
+        org.junit.Assert.assertTrue("Servise Gönderildi", true);
+    }
+
+    @Given("The user enters the protocol number2 of the patient they want to add")
+    public void the_user_enters_the_protocol_number2_of_the_patient_they_want_to_add() {
+        operatingRoomProcedures_page_.popup1protokolNo.click();
+        operatingRoomProcedures_page_.popup1protokolNo.sendKeys("9951101");
+
+    }
+
+    @Given("User clicks on the query button")
+    public void user_clicks_on_the_query_button() {
+        operatingRoomProcedures_page_.protokolNoQuestioningButton.click();
+
+    }
+    @Given("User clicks the service button")
+    public void user_clicks_the_service_button() throws InterruptedException {
+        operatingRoomProcedures_page_.popup1serviceButton.click();
+        Thread.sleep(1000);
+
+    }
+    @Given("User selects the EMERGENCY CHILD DAY CARE Service")
+    public void user_selects_the_EMERGENCY_CHILD_DAY_CARE_service() throws InterruptedException {
+        operatingRoomProcedures_page_.EMERGENCYCHILDDAYCAREServiceButton.click();
+        Thread.sleep(3000);
+
+
+    }
+    @Given("The user clicks Add to patient list button")
+    public void the_user_clicks_add_to_patient_list_button() throws InterruptedException {
+        operatingRoomProcedures_page_.popup1addPatientListButton.click();
+        Thread.sleep(1000);
+
+    }
+
+    @And("the user selects the salon popup add list page")
+    public void theUserSelectsTheSalonPopupAddListPage() throws InterruptedException {
+        operatingRoomProcedures_page_.popup1selectSalonButton.click();
+        Thread.sleep(2000);
+        operatingRoomProcedures_page_.selectSalonButtonFromList.click();
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.popup1addPatientListButton.click();
+
+    }
+
+    @And("The user enters the protocol number of the patient they want to add1")
+    public void theUserEntersTheProtocolNumberOfThePatientTheyWantToAdd() throws InterruptedException {
+        Thread.sleep(2000);
+        operatingRoomProcedures_page_.homePageProtokolButton.click();
+        operatingRoomProcedures_page_.homePageProtokolButton.sendKeys("9951101");
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.findPatient.click();
+
+
+    }
+
+    @And("User verifies that the patient has been added to the list")
+    public void userVerifiesThatThePatientHasBeenAddedToTheList() throws InterruptedException {
+        Assertion assertion = new Assertion();
+        Assert.assertEquals("ACİL ÇOCUK GÜNÜBİRLİK", operatingRoomProcedures_page_.homePageServiceName.getText());
+        org.junit.Assert.assertEquals("ACİL ÇOCUK GÜNÜBİRLİK", false);
+        Thread.sleep(2000);
+    }
+
+    @And("User clicks the exit from the table button")
+    public void userClicksTheExitFromTheTableButton() throws InterruptedException {
+    operatingRoomProcedures_page_.takeOfTableButton.click();
+    Thread.sleep(1000);
+    operatingRoomProcedures_page_.DoYouWantToContinuePopupYESbutton.click();
+     Thread.sleep(1000);
+     operatingRoomProcedures_page_.takeOfTablepageAddButton.click();
+        Thread.sleep(1000);
+    }
+
+    @And("User selects the GUNUBIRLIK KADIN HAST.VE DOGUM SERVISI Service")
+    public void userSelectsTheGUNUBIRLIKKADINHASTVEDOGUMSERVISIService() throws InterruptedException {
+        operatingRoomProcedures_page_.poupServisNameButton.click();
+        operatingRoomProcedures_page_.poupServisNameButton.sendKeys("kadın"+Keys.ENTER);
+        Thread.sleep(1000);
+        operatingRoomProcedures_page_.poupKadınDogumServiceButton.click();
+    }
+
+    @And("User verifies GUNUBIRLIK KADIN HAST.VE DOGUM SERVISI that the patient has been added to the list")
+    public void userVerifiesGUNUBIRLIKKADINHASTVEDOGUMSERVISIThatThePatientHasBeenAddedToTheList() throws InterruptedException {
+        Assertion assertion = new Assertion();
+        Assert.assertEquals("GÜNÜBİRLİK KADIN HAST.VE DOĞUM SERVİSİ", operatingRoomProcedures_page_.homePageServiceName.getText());
+        org.junit.Assert.assertEquals("GÜNÜBİRLİK KADIN HAST.VE DOĞUM SERVİSİ", false);
+        Thread.sleep(2000);
+
+    }
+}
