f7ebb4d95d89a83debfceb3e7485235e01db6c53
fatayt <fatayt@gmail.com>
2023-09-21 22:45:07 +0300

    last update

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources",
         glue = "stepdefinitions",
-        tags = "@DoctorOperatingRoomProcedures",
+        tags = "@OperatingRoomProcedures",
         dryRun = false
 
 )
