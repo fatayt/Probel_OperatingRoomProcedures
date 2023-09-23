3a8301d70a1feee469f5ba4e04e9121b4ad7f32d
fatayt <fatayt@gmail.com>
2023-09-24 01:18:35 +0300

    @SurgeryAppointment

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources",
         glue = "stepdefinitions",
-        tags = "@TC0078",
+        tags = "@Regression",
         dryRun = false
 
 )
