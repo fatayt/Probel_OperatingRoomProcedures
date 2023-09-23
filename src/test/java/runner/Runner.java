e38cd49e3b8ebbb86f359c4d54be539f8d2f3234
faruk uraz <farukuzgun@gmail.com>
2023-09-23 10:45:30 -0400

    23.09.2023

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources",
         glue = "stepdefinitions",
-        tags = "@TR0029",
+        tags = "@TC0083",
         dryRun = false
 
 )
